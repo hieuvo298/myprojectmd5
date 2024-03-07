428938ec65ff314282ec1432b7673a2a2e983bea
hieu <hieuvv29899@gmail.com>
2024-03-07 14:50:21 +0700

    updatetiep

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -12,36 +12,36 @@ import Payment from "./payment.entity";
 import orderItem from "./orderItem";
 
 const createTable = () => {
-//   User.sync().then(() => {
-//     console.log("user created");
-//   });
-//   Category.sync().then(() => {
-//     console.log("category created");
-//   });
-//   Products.sync().then(() => {
-//     console.log("products created");
-//   });
-//   Admin.sync().then(() => {
-//     console.log("admin created");
-//   });
-//   userInfo.sync().then(() => {
-//     console.log("user info created");
-//   });
-//   Payment.sync().then(() => {
-//     console.log("Payment created");
-//   });
-//   orderItem.sync().then(() => {
-//     console.log("orderItem created");
-//   });
-//   productSize.sync().then(() => {
-//     console.log("product size created");
-//   });
-//   Favorite.sync().then(() => {
-//     console.log("favorite created");
-//   });
-//   RateAndComment.sync().then(() => {
-//     console.log("rate and comment created");
-//   });
+  User.sync().then(() => {
+    console.log("user created");
+  });
+  Category.sync().then(() => {
+    console.log("category created");
+  });
+  Products.sync().then(() => {
+    console.log("products created");
+  });
+  Admin.sync().then(() => {
+    console.log("admin created");
+  });
+  userInfo.sync().then(() => {
+    console.log("user info created");
+  });
+  Payment.sync().then(() => {
+    console.log("Payment created");
+  });
+  orderItem.sync().then(() => {
+    console.log("orderItem created");
+  });
+  productSize.sync().then(() => {
+    console.log("product size created");
+  });
+  Favorite.sync().then(() => {
+    console.log("favorite created");
+  });
+  RateAndComment.sync().then(() => {
+    console.log("rate and comment created");
+  });
 };
 
 export default createTable;
