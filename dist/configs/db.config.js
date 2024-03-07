c392ea96e9636016fc4ce499de88745dd93af3e1
hieu <hieuvv29899@gmail.com>
2024-03-07 16:08:42 +0700

    suatip

--- dist/configs/db.config.js
+++ dist/configs/db.config.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize("btq6w1wdtycuv9ftwqxy", "uwe4rcsmmjuzdmdx", "vg9wJZr2a6q8BsLHTTNP", {
-    host: "btq6w1wdtycuv9ftwqxy-mysql.services.clever-cloud.com",
+const sequelize = new sequelize_1.Sequelize("bolnqg7gi4neaymbfrao", "ulwfspvnpvpxmbsf", "Wn2yJTDoUXLaiCJibdHF", {
+    host: "bolnqg7gi4neaymbfrao-mysql.services.clever-cloud.com",
     dialect: "mysql",
 });
 exports.default = sequelize;
